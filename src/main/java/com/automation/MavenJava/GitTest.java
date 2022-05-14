298335009b286ec3be9601f640e5932ef8336b63
roopak <roopaktvns@gmail.com>
2022-05-14 14:09:36 +0530

    This created code is from GitDemo in IST timezone

---
+++ src/main/java/com/automation/MavenJava/GitTest.java
@@ -0,0 +1,15 @@
+package com.automation.MavenJava;
+
+public class GitTest {
+	
+	public static void main(String[] args) {
+		gitDemo();
+		
+	}
+	public static void gitDemo() {
+		System.out.println("My name is GitDemo");
+		System.out.println("Extracted code from GitX");
+
+	}
+
+}
