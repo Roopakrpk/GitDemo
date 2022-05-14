650938442d286783fa27ed0b540723ec2333c614
roopak <roopaktvns@gmail.com>
2022-05-14 22:42:58 +0530

    Architect changes in GitDemo in IST

--- src/main/java/com/automation/MavenJava/GitTest.java
+++ src/main/java/com/automation/MavenJava/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 	
 	public static void main(String[] args) {
 		gitDemo();
+		gitDemo1();
 		
 	}
 	public static void gitDemo() {
@@ -11,7 +12,13 @@ public class GitTest {
 		System.out.println("Extracted code from GitX");
 		System.out.println("Im an US guy working in EST timezone");
 		System.out.println("For GitTest");
+		System.out.println("My name changed to GitDemo develop");
 
+	}
+	public static void gitDemo1() {
+		System.out.println("Im an architect");
+		System.out.println("My name is Archi");
+		
 	}
 
 }
--- src/test/java/com/automation/MavenJava/GitTest1.java
+++ src/test/java/com/automation/MavenJava/GitTest1.java
@@ -4,6 +4,7 @@ public class GitTest1 {
 	
 	public static void main(String[] args) {
 		gitDemo();
+		gitDemo1();
 		
 	}
 	public static void gitDemo() {
@@ -11,6 +12,13 @@ public class GitTest1 {
 		System.out.println("Extracted code from GitDemo");
 		System.out.println("Im an Asian guy working in IST timezone");
 		System.out.println("For GitTest1");
+		System.out.println("Change to develop branch");
+	}
+	
+	public static void gitDemo1() {
+		System.out.println("Lives in Asian timezone");
+		System.out.println("Working in framework architecture");
+		
 	}
 
 }
